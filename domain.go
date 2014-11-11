23894176b349b80e9d818c161fc590651226654f
Jay Wren <jrwren@xmtp.net>
2014-11-10 19:43:27 -0500

    add test

--- domain.go
+++ domain.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+// GetDomains retrieves all the domains for a given account.
 func (c *Client) GetDomains() ([]DomainResponse, error) {
 	req, err := c.NewRequest(nil, "GET", "/domains")
 	if err != nil {
@@ -38,20 +39,20 @@ type DomainResponse struct {
 }
 
 type Domain struct {
-	Id             int        `json:"id"`
-	UserId         int        `json:"user_id"`
-	RegistrantId   *int       `json:"registrant_id"`
-	Name           string     `json:"name"`
-	UnicodeName    string     `json:"unicode_name"`
-	Token          string     `json:"token"`
-	State          string     `json:"state"`
-	Language       string     `json:"language"`
-	Lockable       bool       `json:"lockable"`
-	AutoRenew      bool       `json:"auto_renew"`
-	WhoisProtected bool       `json:"whois_protected"`
-	RecordCount    int        `json:"record_count"`
-	ServiceCount   int        `json:"service_count"`
-	ExpiresOn      string     `json:"expires_on"`
-	CreatedAt      *time.Time `json:"created_at"`
-	UpdatedAt      *time.Time `json:"updated_at"`
+	Id             int       `json:"id"`
+	UserId         int       `json:"user_id"`
+	RegistrantId   int       `json:"registrant_id"`
+	Name           string    `json:"name"`
+	UnicodeName    string    `json:"unicode_name"`
+	Token          string    `json:"token"`
+	State          string    `json:"state"`
+	Language       string    `json:"language"`
+	Lockable       bool      `json:"lockable"`
+	AutoRenew      bool      `json:"auto_renew"`
+	WhoisProtected bool      `json:"whois_protected"`
+	RecordCount    int       `json:"record_count"`
+	ServiceCount   int       `json:"service_count"`
+	ExpiresOn      string    `json:"expires_on"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
